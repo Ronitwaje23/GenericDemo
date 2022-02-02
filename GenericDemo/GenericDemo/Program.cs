81078443525f87bdb3dee69c501b6a84df9a0ddf
ronit#123 <ronitwaje23>
2022-02-02 15:51:32 +0530

    comparision of generic

---
+++ GenericDemo/GenericDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three numbers:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int max = Generic.FindMaxIntNumber(a, b, c);
+            if (max == 0)
+
+            {
+                Console.WriteLine("All three numbers are equal!!!");
+            }
+            else
+            {
+
+                Console.WriteLine("Maximum among three:" + max);
+                Console.ReadLine();
+                
+
+                }
+            }
+        }
+    }
+
