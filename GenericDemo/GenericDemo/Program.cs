ce2204386b1c898f2291a0c7b8211db9ddaff255
ronit#123 <ronitwaje23>
2022-02-02 18:33:19 +0530

    use of generic

--- GenericDemo/GenericDemo/Program.cs
+++ GenericDemo/GenericDemo/Program.cs
@@ -8,24 +8,20 @@ namespace GenericDemo
 {
     internal class Program
     {
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter three words:");
             
-            int[] arr = { 1, 2, 3, 4, 5 };
-            GenericMaximum<int> obj = new GenericMaximum<int>(arr);
-            obj.PrintMaxValueOfArray();
-            string[] Stringarr = { "abc", "efg", "hij" };
+            {
+                
 
-            GenericMaximum<string> obj1 = new GenericMaximum<string>(Stringarr);
-            obj1.PrintMaxValueOfArray();
-            char[] Chararr = { 'a', 'b', 'c', };
-            GenericMaximum<char> obj2 = new GenericMaximum<char>(Chararr);
-            obj2.PrintMaxValueOfArray();
-            float[] Floatarr = { 1.1f, 1.2f, 1.3f };
-            GenericMaximum<float> obj3 = new GenericMaximum<float>(Floatarr);
-            obj3.PrintMaxValueOfArray();
-            Console.ReadLine();
+                GenericMaximum<int> first = new GenericMaximum<int>();
+                first.FindMax(1, 2, 5);
+
+
+
+                Console.ReadLine();
+            }
         }
+
     }
 }
